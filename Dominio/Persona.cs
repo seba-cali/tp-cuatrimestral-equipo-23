dc8aacebf161a8045a69e028f35563c3920f541b
seba-cali <sebastian.calicanton@alumnos.frgp.utn.edu.ar>
2023-06-13 16:22:54 -0300

    Solución y Clase Persona

---
+++ Dominio/Persona.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+	public class Persona
+	{
+		public int id { get; set; }
+		public int dni { get; set; } 
+		public string nombres { get; set; }
+		public string apellidos { get; set; }
+		public string direccion { get; set; }
+		public string telefono { get; set; }
+		public string email { get; set; }
+		public DateTime fechaNacimiento { get; set; }
+		public string sexo { get; set; }
+		public bool estado { get; set; }
+	}
+}
