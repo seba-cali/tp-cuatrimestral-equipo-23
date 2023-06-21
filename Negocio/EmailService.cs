3f21bdb36ee08ddb1b86b10b44f66abb41f9ea82
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-06-21 00:29:49 -0300

    fix: send email register and add template email

--- Negocio/EmailService.cs
+++ Negocio/EmailService.cs
@@ -12,10 +12,13 @@ namespace Negocio
         public EmailService()
         {
             server = new SmtpClient();
-            server.Credentials = new NetworkCredential("8f21cfba9301b4", "1e3abbd19c7280");
+            //server.Credentials = new NetworkCredential("8f21cfba9301b4", "1e3abbd19c7280");
+            server.Credentials = new NetworkCredential("nico.river09.9@gmail.com", "MfOG8dc0FkrZPhAI");
             server.EnableSsl = true;
-            server.Port = 2525;
-            server.Host = "sandbox.smtp.mailtrap.io";
+            server.Port = 587;
+            //server.Port = 2525;
+            server.Host = "smtp-relay.sendinblue.com";
+            //server.Host = "smtp.mailtrap.io";
            // server.Host = "smtp.mailtrap.io";
         }
 
